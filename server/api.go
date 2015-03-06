b7b26921422014a8cd40e02fed536de939000bfb
Yuichi Shiwaku <yuichi.shiwaku@gmail.com>
2015-03-06 13:58:31 +0900

    コミット漏れてた・・・

---
+++ server/api.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/zenazn/goji/web"
+	"github.com/shiwork/ppserver/ppserver"
+)
+
+func SampleApi(c web.C, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.URL.Scheme == "" {
+		r.URL.Scheme = "http"
+	}
+	movies := &[]ppserver.Movie{
+		ppserver.Movie{
+			Url: r.URL.Scheme + "://" + r.Host + "/sample_movie.mp4",
+			Title: "PPServer Sample",
+		},
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(movies)
+}
